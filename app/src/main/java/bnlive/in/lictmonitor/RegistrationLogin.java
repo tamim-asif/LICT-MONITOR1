1fbd3202b1cdcf6f83b327d3513fe107d4e7029b
Sk. Faisal <faisalex35@gmail.com>
2018-03-29 16:27:43 +0600

    RegistrationLogin page actionbar problem solved!

--- app/src/main/java/bnlive/in/lictmonitor/RegistrationLogin.java
+++ app/src/main/java/bnlive/in/lictmonitor/RegistrationLogin.java
@@ -45,7 +45,7 @@ public class RegistrationLogin extends AppCompatActivity {
         setContentView(R.layout.activity_reglogin);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
